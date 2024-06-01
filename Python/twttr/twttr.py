77bb6909b76bf1c2d811d37952100fb01a7277f6
Sizwe Chris Khomo <164577349+cskhomo@users.noreply.github.com>
2024-06-01 14:49:14 +0000

    Sat, Jun 1, 2024, 4:49 PM +02:00

--- Python/twttr/twttr.py
+++ Python/twttr/twttr.py
@@ -5,14 +5,15 @@ def main():
 def twttr():
 
     message = input("Input: ")
+    formatted_message = message.lower()
+
     vowels = ["a", "e", "i", "o", "u"]
     tweet = str()
 
-    for alpha in message:
-        if alpha.lower() not it vowels:
+    for alpha in formatted_message:
+        if alpha not in vowels:
             tweet += alpha
 
     print(f"Output: {tweet}")
-
 
 main()
