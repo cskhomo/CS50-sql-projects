7434910ef369a018ba8f858874a1b981cce805da
Sizwe Chris Khomo <164577349+cskhomo@users.noreply.github.com>
2024-06-11 07:42:46 +0000

    Tue, Jun 11, 2024, 9:42 AM +02:00

--- WeThinkcode/count.py
+++ WeThinkcode/count.py
@@ -1,9 +1,11 @@
 def count_words(string):
 
-    list_of_words = string.split(" ")
-    number_of_words = len(list_of_words)
+    # list_of_words = string.split(" ")
+    # number_of_words = len(list_of_words)
 
-    print(list_of_words)
-    return number_of_words
+    # print(list_of_words)
+    # return number_of_words
 
-print(count_words("This sentence has  2 spaces"))
+    pass
+
+print(("me and     You").strip())
