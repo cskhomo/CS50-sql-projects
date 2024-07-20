030324b33f4538720f982af532105a54d204fdf9
Sizwe Chris Khomo <164577349+cskhomo@users.noreply.github.com>
2024-07-20 09:28:17 +0000

    Sat, Jul 20, 2024, 11:28 AM +02:00

--- WeThinkcode/count.py
+++
@@ -1,7 +0,0 @@
-def count_words(string):
-
-    list_of_words = string.split()
-    print(list_of_words)
-    return len(list_of_words)
-
-print(count_words("This Is A  Sentence"))
--- WeThinkcode/lambda.py
+++
@@ -1,3 +0,0 @@
-add_one = lambda x: x + 1
-
-print(add_one(2))
