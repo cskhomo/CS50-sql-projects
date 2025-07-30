44be8eef5860ba81760409103c9792f78a803d68
Sizwe Chris Khomo <164577349+cskhomo@users.noreply.github.com>
2025-07-30 15:59:21 +0000

    Wed, Jul 30, 2025, 5:59 PM +02:00

--- C/mario-less/mario.c
+++ C/mario-less/mario.c
@@ -2,8 +2,17 @@
 #include <cs50.h>
 
 
+void mario(int height);
 
 int main(void){
 
     int height = get_int("Height: ");
+    mario(height);
+}
+
+void mario(int height){
+
+    for (int i=0; i<height; i++){
+        printf("#");
+    }
 }
